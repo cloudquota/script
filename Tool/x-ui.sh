40b3fd83b7b98da3086e44ee8819f1b577523811
cloudquota <63286327+cloudquota@users.noreply.github.com>
2025-07-31 12:08:23 +0800

    Update x-ui.sh

--- Tool/x-ui.sh
+++ Tool/x-ui.sh
@@ -2,174 +2,97 @@
 
 red='\033[0;31m'
 green='\033[0;32m'
+blue='\033[0;34m'
 yellow='\033[0;33m'
 plain='\033[0m'
 
-cur_dir=$(pwd)
+[[ $EUID -ne 0 ]] && echo -e "${red}Fatal error: ${plain}请用 root 权限运行脚本！" && exit 1
 
-# check root
-[[ $EUID -ne 0 ]] && echo -e "${red}错误：${plain} 必须使用root用户运行此脚本！\n" && exit 1
-
-# check os
-if [[ -f /etc/redhat-release ]]; then
-    release="centos"
-elif cat /etc/issue | grep -Eqi "debian"; then
-    release="debian"
-elif cat /etc/issue | grep -Eqi "ubuntu"; then
-    release="ubuntu"
-elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
-    release="centos"
-elif cat /proc/version | grep -Eqi "debian"; then
-    release="debian"
-elif cat /proc/version | grep -Eqi "ubuntu"; then
-    release="ubuntu"
-elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
-    release="centos"
-else
-    echo -e "${red}未检测到系统版本，请联系脚本作者！${plain}\n" && exit 1
-fi
-
-arch=$(arch)
-
-if [[ $arch == "x86_64" || $arch == "x64" || $arch == "amd64" ]]; then
-    arch="amd64"
-elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
-    arch="arm64"
-elif [[ $arch == "s390x" ]]; then
-    arch="s390x"
-else
-    arch="amd64"
-    echo -e "${red}检测架构失败，使用默认架构: ${arch}${plain}"
-fi
-
-echo "架构: ${arch}"
-
-if [ $(getconf WORD_BIT) != '32' ] && [ $(getconf LONG_BIT) != '64' ]; then
-    echo "本软件不支持 32 位系统(x86)，请使用 64 位系统(x86_64)，如果检测有误，请联系作者"
-    exit -1
-fi
-
-os_version=""
-
-# os version
+# 检测系统类型
 if [[ -f /etc/os-release ]]; then
-    os_version=$(awk -F'[= ."]' '/VERSION_ID/{print $3}' /etc/os-release)
-fi
-if [[ -z "$os_version" && -f /etc/lsb-release ]]; then
-    os_version=$(awk -F'[= ."]+' '/DISTRIB_RELEASE/{print $2}' /etc/lsb-release)
-fi
-
-if [[ x"${release}" == x"centos" ]]; then
-    if [[ ${os_version} -le 6 ]]; then
-        echo -e "${red}请使用 CentOS 7 或更高版本的系统！${plain}\n" && exit 1
-    fi
-elif [[ x"${release}" == x"ubuntu" ]]; then
-    if [[ ${os_version} -lt 16 ]]; then
-        echo -e "${red}请使用 Ubuntu 16 或更高版本的系统！${plain}\n" && exit 1
-    fi
-elif [[ x"${release}" == x"debian" ]]; then
-    if [[ ${os_version} -lt 8 ]]; then
-        echo -e "${red}请使用 Debian 8 或更高版本的系统！${plain}\n" && exit 1
-    fi
+    source /etc/os-release
+    release=$ID
+else
+    echo "无法检测系统类型，请手动确认。" >&2
+    exit 1
 fi
 
+# 检测架构
+arch() {
+    case "$(uname -m)" in
+        x86_64 | x64 | amd64) echo 'amd64' ;;
+        i*86 | x86) echo '386' ;;
+        armv8* | armv8 | arm64 | aarch64) echo 'arm64' ;;
+        armv7* | armv7 | arm) echo 'armv7' ;;
+        *) echo -e "${red}不支持的架构！${plain}" && exit 1 ;;
+    esac
+}
+
+# 检查 GLIBC >= 2.32
+check_glibc_version() {
+    glibc_version=$(ldd --version | head -n1 | awk '{print $NF}')
+    required_version="2.32"
+    if [[ "$(printf '%s\n' "$required_version" "$glibc_version" | sort -V | head -n1)" != "$required_version" ]]; then
+        echo -e "${red}GLIBC 版本过低 (${glibc_version})，需要 >= 2.32${plain}"
+        exit 1
+    fi
+}
+check_glibc_version
+
+# 安装依赖
 install_base() {
-    if [[ x"${release}" == x"centos" ]]; then
-        yum install wget curl tar -y
-    else
-        apt install wget curl tar -y
-    fi
+    case "${release}" in
+        ubuntu | debian | armbian) apt update && apt install -y wget curl tar tzdata ;;
+        centos | rhel | almalinux | rocky | ol) yum update -y && yum install -y wget curl tar tzdata ;;
+        fedora | amzn | virtuozzo) dnf update -y && dnf install -y wget curl tar tzdata ;;
+        arch | manjaro | parch) pacman -Syu --noconfirm wget curl tar tzdata ;;
+        *) apt update && apt install -y wget curl tar tzdata ;;
+    esac
 }
 
-#This function will be called when user installed x-ui out of sercurity
-config_after_install() {
-    echo -e "${yellow}为了安全起见，安装/更新完成后将自动修改端口与账户密码${plain}"
-    
-    # 设置为您想要的值
-    config_account="admin"
-    config_password="19991126."
-    config_port="54321"
-
-    echo -e "${yellow}您的账户名将设定为:${config_account}${plain}"
-    echo -e "${yellow}您的账户密码将设定为:${config_password}${plain}"
-    echo -e "${yellow}您的面板访问端口将设定为:${config_port}${plain}"
-    echo -e "${yellow}确认设定,设定中${plain}"
-    
-    /usr/local/x-ui/x-ui setting -username ${config_account} -password ${config_password}
-    echo -e "${yellow}账户密码设定完成${plain}"
-    /usr/local/x-ui/x-ui setting -port ${config_port}
-    echo -e "${yellow}面板端口设定完成${plain}"
-}
-
-
-install_x-ui() {
-    systemctl stop x-ui
+# 安装并配置 x-ui
+install_x_ui() {
     cd /usr/local/
+    arch_str=$(arch)
 
-    if [ $# == 0 ]; then
-        last_version=$(curl -Ls "https://api.github.com/repos/vaxilu/x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-        if [[ ! -n "$last_version" ]]; then
-            echo -e "${red}检测 x-ui 版本失败，可能是超出 Github API 限制，请稍后再试，或手动指定 x-ui 版本安装${plain}"
-            exit 1
-        fi
-        echo -e "检测到 x-ui 最新版本：${last_version}，开始安装"
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz https://github.com/vaxilu/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz
-        if [[ $? -ne 0 ]]; then
-            echo -e "${red}下载 x-ui 失败，请确保你的服务器能够下载 Github 的文件${plain}"
-            exit 1
-        fi
-    else
-        last_version=$1
-        url="https://github.com/vaxilu/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz"
-        echo -e "开始安装 x-ui v$1"
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz ${url}
-        if [[ $? -ne 0 ]]; then
-            echo -e "${red}下载 x-ui v$1 失败，请确保此版本存在${plain}"
-            exit 1
-        fi
-    fi
+    # 获取最新版 tag
+    tag_version=$(curl -sL "https://api.github.com/repos/MHSanaei/3x-ui/releases/latest" | grep '"tag_name":' | cut -d\" -f4)
+    [ -z "$tag_version" ] && echo -e "${red}获取版本失败，请检查网络${plain}" && exit 1
 
-    if [[ -e /usr/local/x-ui/ ]]; then
-        rm /usr/local/x-ui/ -rf
-    fi
+    echo -e "${green}下载 x-ui ${tag_version}...${plain}"
+    wget -N -O x-ui-linux-${arch_str}.tar.gz https://github.com/MHSanaei/3x-ui/releases/download/${tag_version}/x-ui-linux-${arch_str}.tar.gz || exit 1
+    wget -O /usr/bin/x-ui-temp https://raw.githubusercontent.com/MHSanaei/3x-ui/main/x-ui.sh
 
-    tar zxvf x-ui-linux-${arch}.tar.gz
-    rm x-ui-linux-${arch}.tar.gz -f
+    # 停止服务并清理旧文件
+    systemctl stop x-ui 2>/dev/null
+    rm -rf /usr/local/x-ui/
+    tar zxvf x-ui-linux-${arch_str}.tar.gz
+    rm -f x-ui-linux-${arch_str}.tar.gz
+
     cd x-ui
-    chmod +x x-ui bin/xray-linux-${arch}
+    chmod +x x-ui x-ui.sh bin/xray-linux-${arch_str}
+    mv /usr/bin/x-ui-temp /usr/bin/x-ui && chmod +x /usr/bin/x-ui
+
+    # 安装 systemd 服务
     cp -f x-ui.service /etc/systemd/system/
-    wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/vaxilu/x-ui/main/x-ui.sh
-    chmod +x /usr/local/x-ui/x-ui.sh
-    chmod +x /usr/bin/x-ui
-    config_after_install
-    #echo -e "如果是全新安装，默认网页端口为 ${green}54321${plain}，用户名和密码默认都是 ${green}admin${plain}"
-    #echo -e "请自行确保此端口没有被其他程序占用，${yellow}并且确保 54321 端口已放行${plain}"
-    #    echo -e "若想将 54321 修改为其它端口，输入 x-ui 命令进行修改，同样也要确保你修改的端口也是放行的"
-    #echo -e ""
-    #echo -e "如果是更新面板，则按你之前的方式访问面板"
-    #echo -e ""
     systemctl daemon-reload
     systemctl enable x-ui
     systemctl start x-ui
-    echo -e "${green}x-ui v${last_version}${plain} 安装完成，面板已启动，"
-    echo -e ""
-    echo -e "x-ui 管理脚本使用方法: "
-    echo -e "----------------------------------------------"
-    echo -e "x-ui              - 显示管理菜单 (功能更多)"
-    echo -e "x-ui start        - 启动 x-ui 面板"
-    echo -e "x-ui stop         - 停止 x-ui 面板"
-    echo -e "x-ui restart      - 重启 x-ui 面板"
-    echo -e "x-ui status       - 查看 x-ui 状态"
-    echo -e "x-ui enable       - 设置 x-ui 开机自启"
-    echo -e "x-ui disable      - 取消 x-ui 开机自启"
-    echo -e "x-ui log          - 查看 x-ui 日志"
-    echo -e "x-ui v2-ui        - 迁移本机器的 v2-ui 账号数据至 x-ui"
-    echo -e "x-ui update       - 更新 x-ui 面板"
-    echo -e "x-ui install      - 安装 x-ui 面板"
-    echo -e "x-ui uninstall    - 卸载 x-ui 面板"
-    echo -e "----------------------------------------------"
+
+    # 自动配置
+    /usr/local/x-ui/x-ui setting -username "admin" -password "19991126." -port 54321 -webBasePath ""
+    /usr/local/x-ui/x-ui migrate
+
+    # 显示信息
+    ip=$(curl -s --max-time 3 https://api.ipify.org || echo "<你的IP>")
+    echo -e "\n${green}x-ui ${tag_version} 安装完成并运行中...${plain}"
+    echo -e "${green}=========================================${plain}"
+    echo -e "${green}访问地址：http://${ip}:54321/${plain}"
+    echo -e "${green}用户名：admin${plain}"
+    echo -e "${green}密码：19991126${plain}"
+    echo -e "${green}=========================================${plain}"
 }
 
-echo -e "${green}开始安装${plain}"
+echo -e "${green}开始安装 x-ui ...${plain}"
 install_base
-install_x-ui $1
+install_x_ui
